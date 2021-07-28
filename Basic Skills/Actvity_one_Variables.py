f91863cab89e47c2955778390e960ee76660ee95
Paul <no21479@gmail.com>
2021-07-28 14:01:30 +1200

    ...

---
+++ Basic Skills/Actvity_one_Variables.py
@@ -0,0 +1,18 @@
+#Program ask for username, favorite color and two favorite numbers
+#Program is going to output a greeting and will do some maths
+
+#This indicates the use of variables
+
+#Created by Super_Fusiom (Will be using Github name)
+#Created on 28/7/21
+
+#Ask user for name
+name
+
+#Ask user for favourite color
+
+#Ask user for favorurite numbers
+
+#Do the Maths
+
+#Output Greeting and print results
--- Basic Skills/test.py
+++ Basic Skills/test.py
@@ -1,11 +1,8 @@
-#Test
-mep = 5
-bing = 10 
-bong = 15
-if bong != mep:
-    print(bing)
-if mep > bong:
-    print("coconut")
-else:
-    print("HAAHAHAHAHAHAHAHAHAHAHAHA")
+#This program is my first program I'm writing in class as I've done this years ago
+#Created on 28/7/21
+#Created by Paul Singh
+#Github: Super_Fusiom
 
+print ("Bing bing bong")
+print ("Waiting for varibles")
+print ("ZZZZZZZZZZZZZZZZZZZZZ")
