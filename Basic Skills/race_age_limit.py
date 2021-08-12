a08be390ffd07ee2f7e02c433ece8e75c28dd8fe
Paul <no21479@gmail.com>
2021-08-12 21:51:21 +1200

    if with ANDS

--- Basic Skills/race_age_limit.py
+++ Basic Skills/race_age_limit.py
@@ -1,3 +1,6 @@
+"""If age is less or equal to 10 years, 
+race length is 100m
+else the race length is 200m"""
 age = int(input("How old are you? "))
 if age <= 10:
     print('The race length is 100m')
