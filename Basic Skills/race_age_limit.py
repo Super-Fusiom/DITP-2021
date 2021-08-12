0741d476d59b65fbfeb0127e896363e7bfd9689f
Paul <no21479@gmail.com>
2021-08-12 21:17:28 +1200

    Race go brrrrr

---
+++ Basic Skills/race_age_limit.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you? "))
+if age <= 10:
+    print('The race length is 100m')
+    race_length = 100
+else:
+    print('The race lenght is 200m')
+    race_length = 200
+print("All competitors must run the race distance")
